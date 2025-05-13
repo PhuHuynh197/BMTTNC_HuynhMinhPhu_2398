f4401136a1ff4d570544006ca21c91d991fce72d
PhuHuynh197 <huynhminhphu197@gmail.com>
2025-05-13 16:03:19 +0700

    [add] lab_01 ex03

---
+++ lab_01/ex03/ex03_02.py
@@ -0,0 +1,6 @@
+def dao_nguoc_list(lst):
+    return lst[::-1]
+input_list = input("Nhap danh sach so nguyen (cach nhau boi phay): ")
+numbers = list(map(int, input_list.split(',')))
+list_dao_nguoc = dao_nguoc_list(numbers)
+print("List dao nguoc la:", list_dao_nguoc)
