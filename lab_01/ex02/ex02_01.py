f2cd3a750f9ffe92bec3e4b6ce1e122624713c76
PhuHuynh197 <huynhminhphu197@gmail.com>
2025-06-03 12:37:36 +0700

    Delete lab_01 directory

--- lab_01/ex02/ex02_01.py
+++
@@ -1,3 +0,0 @@
-ten = input("Nhap ten cua ban: ")
-tuoi = input("Nhap tuoi cua ban: ")
-print("Chao Mung,",ten,"! Ban",tuoi,"tuoi")
--- lab_01/ex03/ex03_02.py
+++
@@ -1,6 +0,0 @@
-def dao_nguoc_list(lst):
-    return lst[::-1]
-input_list = input("Nhap danh sach so nguyen (cach nhau boi phay): ")
-numbers = list(map(int, input_list.split(',')))
-list_dao_nguoc = dao_nguoc_list(numbers)
-print("List dao nguoc la:", list_dao_nguoc)
